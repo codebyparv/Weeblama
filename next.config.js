8731ac22ddb63e50d3cd402ad68b676debb11926
Johnathon Frederick <academics@mailfence.com>
2024-07-10 02:26:18 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  basePath: "/Weeblama",
-  output: "export",  // <=== enables static exports
-};
-
-module.exports = nextConfig;
